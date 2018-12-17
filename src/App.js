b989dbf086942e09f8efe851d861afa48991c89b
Miguel Torres <miguel@alturasoluciones.com>
2018-12-17 12:58:53 -0500

    With context API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component, createContext } from 'react'
 import './App.css'
 import { Button } from 'react-bootstrap'
 
+const ThemeContext = createContext('light')
+
 class App extends Component {
   render () {
-    return <Toolbar theme="dark"/>
+    return (
+      <ThemeContext.Provider value="dark">
+        <Toolbar/>
+      </ThemeContext.Provider>
+    )
   }
 }
 
 function Toolbar (props) {
   return (
     <div>
-      <ThemedButton theme={props.theme}/>
+      <ThemedButton/>
     </div>
   )
 }
 
 class ThemedButton extends Component {
+  // Assign a contextType to read the current theme context.
+  static contextType = ThemeContext
+
   render () {
-    return <Button>{this.props.theme}</Button>
+    return <Button>{this.context}</Button>
   }
 }
 
