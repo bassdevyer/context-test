9e3e5e245e683e64baf2ab15cfafb3349e339829
Miguel Torres <miguel@alturasoluciones.com>
2018-12-17 12:54:09 -0500

    Without context API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { Button } from 'react-bootstrap'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  render () {
+    return <Toolbar theme="dark"/>
   }
 }
 
-export default App;
+function Toolbar (props) {
+  return (
+    <div>
+      <ThemedButton theme={props.theme}/>
+    </div>
+  )
+}
+
+class ThemedButton extends Component {
+  render () {
+    return <Button>{this.props.theme}</Button>
+  }
+}
+
+export default App
